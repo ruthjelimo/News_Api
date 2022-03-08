8f2c2de71e4ed7f85d238ef64ee8101cfd10a0bf
ruthjelimo <ruthjmimo@gmail.com>
2022-03-09 02:16:06 +0300

    updated files

---
+++ app/models.py
@@ -0,0 +1,14 @@
+class News:
+
+
+    def __init__(self, source_name, author, title, description, url, urlToImage, publishedAt, content):
+
+        
+        self.source_name = source_name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
