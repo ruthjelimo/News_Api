9980fe9f23375506c0f2aeeeec3b52c2eaa5a435
ruthjelimo <ruthjmimo@gmail.com>
2022-03-09 07:55:20 +0300

    updated files

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,36 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template,redirect,url_for
 from . import main
-from ..request import get_news
-from ..models import News
+from ..request import get_sources, get_headlines,get_category,article_source
 
-@main.route("/")
+
+#Views 
+@main.route('/')
 def index():
+    '''
+    Root function returning index/home page with data
+    '''
+    source = get_sources()
+    headlines = get_headlines()
+    return render_template('index.html',sources=source, headlines = headlines)
 
-    title = 'Home - The hottest news around the globe'
-    top_news = get_news('articles')
-    print(top_news)
-    return render_template('index.html', title=title, top_news = top_news)
+@main.route('/articles/<id>')
+def article(id):
+
+    '''
+    View article page function that returns the various article details page and its data
+    '''
+    # title= 'Articles'
+    articles = article_source(id)
+    title = f'{id}'
+    
+    return render_template('articles.html',articles= articles,title=title )
+@main.route('/categories/<category>')
+def category(category):
+    '''
+    function to return the categories.html page and its content
+    '''
+    category = get_category(category)
+    title = f'{category}'
+    category = category
+
+    return render_template('categories.html',title = title,category = category )
