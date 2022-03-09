c257849ae6ddbd343d0a75e6f5d9e56432e15b21
ruthjelimo <ruthjmimo@gmail.com>
2022-03-09 08:09:33 +0300

    added files

--- app/models.py
+++ app/models.py
@@ -1,14 +1,49 @@
-class News:
+class Sources:
 
+    '''
+    source class to define Objects
+    '''
 
-    def __init__(self, source_name, author, title, description, url, urlToImage, publishedAt, content):
-
-        
-        self.source_name = source_name
-        self.author = author
-        self.title = title
+    def __init__(self,id,name,description,url):
+        self.id= id
+        self.name = name
         self.description = description
         self.url = url
-        self.urlToImage = urlToImage
-        self.publishedAt = publishedAt
+
+class Headlines:
+    '''
+    class that define instance of headlines
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
+class Article:
+    '''
+    Class that instantiates objects of the news article objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title,content):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
         self.content = content
+
+
+
+class Category:
+    '''
+    Class that instantiates objects of the news categories objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
